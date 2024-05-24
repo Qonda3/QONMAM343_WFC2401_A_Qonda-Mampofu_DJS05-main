675afc196bfc52b6f9dc50d0206631900872d479
Qonda <138157862+Qonda3@users.noreply.github.com>
2024-05-24 14:30:05 +0200

    Implement reducer function for state management

---
+++ reducer.js
@@ -0,0 +1,18 @@
+import { ADD, SUBTRACT, RESET } from './actions';
+
+const initialState = { count: 0 };
+
+const reducer = (state = initialState, action) => {
+    switch (action.type) {
+        case ADD:
+            return { count: state.count + 1 };
+        case SUBTRACT:
+            return { count: state.count - 1 };
+        case RESET:
+            return { count: 0 };
+        default:
+            return state;
+    }
+};
+
+export default reducer;
