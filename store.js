830145675fe14e50bdcf00be4412392142b0243f
Qonda <138157862+Qonda3@users.noreply.github.com>
2024-05-24 14:34:13 +0200

    Implement Redux-inspired store

---
+++ store.js
@@ -0,0 +1,25 @@
+class Store {
+    constructor(reducer, initialState) {
+        this.reducer = reducer;
+        this.state = initialState;
+        this.listeners = [];
+    }
+
+    getState() {
+        return this.state;
+    }
+
+    dispatch(action) {
+        this.state = this.reducer(this.state, action);
+        this.listeners.forEach(listener => listener());
+    }
+
+    subscribe(listener) {
+        this.listeners.push(listener);
+        return () => {
+            this.listeners = this.listeners.filter(l => l !== listener);
+        };
+    }
+}
+
+export default Store;
